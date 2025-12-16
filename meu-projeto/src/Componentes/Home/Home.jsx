5642d3916a668f24bbb8b0aa2666ae575402f56a
Samuel Ferreira Lopes <samelferreiralopes6@gmail.com>
2025-12-16 16:36:42 -0300

    home em construção

--- meu-projeto/src/Componentes/Home/Home.jsx
+++ meu-projeto/src/Componentes/Home/Home.jsx
@@ -6,8 +6,14 @@ function Home(props) {
     return (
         <>
             <section className="container-home">
-                <div className="card-home">
+                <div className="card card-home">
                     <h1>{props.titulo}</h1>
+                </div>
+                <div className="card card-opcoes">
+                    <h2>Navegue entre algumas sessões do site</h2>
+                    <button></button>
+                    <button></button>
+                    <button></button>
                 </div>
             </section>
         </>
